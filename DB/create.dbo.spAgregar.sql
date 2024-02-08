8f33bd42dd7074f136b7e23834d00087b0d2f2d9
jfernando.lirap <jfernando.lirap@gmail.com>
2024-02-08 17:03:15 -0600

    # 2
    Corregidas commillas dobles por comillas simples en script de creacion de stored procedure spAgregar

--- DB/create.dbo.spAgregar.sql
+++ DB/create.dbo.spAgregar.sql
@@ -12,7 +12,7 @@ BEGIN
 	BEGIN TRY
 		INSERT INTO BdiExamen.dbo.tblExamen (idExamen, Nombre, Decripcion)
 		VALUES (@ID, @Nombre, @Descripcion);
-        SELECT 0 AS ReturnCode, "Registro insertado satisfactoriamente" AS ErrorDescription
+        SELECT 0 AS ReturnCode, 'Registro insertado satisfactoriamente' AS ErrorDescription
 	END TRY
 	BEGIN CATCH
 		SELECT @@ERROR AS ReturnCode, ERROR_MESSAGE() AS ErrorDescription
